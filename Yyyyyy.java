88d4f61bf333f00ff29106647b6bc181af7ae729
nazifa-codes <nazifatasnimsandhi@gmail.com>
2025-10-04 10:39:26 +0600

    initial commit : User doing the SUM in JAVA

---
+++ Yyyyyy.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yyyyyy;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class Yyyyyy {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number a :");
+        int a=sc.nextInt();
+        sc.nextLine();
+        
+        System.out.print("Enter second number b :");
+        int b = sc.nextInt();
+        
+         ooppp o = new ooppp(a, b); // Only pass a and b
+         int sum = o.doTheMath();   // Let the method calculate and return the sum
+
+
+      
+        
+    }
+}
---
+++ ooppp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.yyyyyy;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ooppp {
+    public int a;
+    public int b;
+    public int sum;
+
+    public ooppp(int a, int b ) {
+        this.a = a;
+        this.b = b;
+         
+    }
+    public int doTheMath(){
+        sum=a+b;
+        System.out.println("a"+a+"+"+"b"+b+"=sum"+sum);
+        return sum;
+    }
+    
+}
